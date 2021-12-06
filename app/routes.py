d4ce387b396009ab93f784228bef4fbb0e2caf0d
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2021-12-06 15:23:50 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template
 from app import app
+import requests
 
+app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title="Codecapsules")
+@app.route("/")
+def home():
+    print("getting")
+    r = requests.get("https://loadshedding.eskom.co.za/LoadShedding/GetStatus").text
+    print(r)
+    return r
