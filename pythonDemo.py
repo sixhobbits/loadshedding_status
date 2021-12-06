9a922ae7d9b8c107e131b6f823b5f42a1697d665
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2021-12-06 15:29:20 +0200

    loadshedding

--- pythonDemo.py
+++ pythonDemo.py
@@ -1,1 +1,15 @@
-from app import app
+from flask import Flask, render_template
+import requests
+
+print("building app")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("getting")
+    r = requests.get("https://loadshedding.eskom.co.za/LoadShedding/GetStatus").text
+    print(r)
+    return r
+
+if __name__ == '__main__':
+    app.run(debug=True)
